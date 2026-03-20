586d966d65c4066b94f0a12b04ff4dc9f805154c
Roni Tuohino <rt@ronituohino.com>
2026-03-20 10:35:07 +0200

    Read spec, and start proper refactor

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -15,41 +15,49 @@ export class Shop {
     this.items = items;
   }
 
+  updateItem(item) {
+    if (item.name === Shop.SULFURAS) {
+      return item;
+    }
+
+    if (![Shop.AGED_BRIE, Shop.SULFURAS, Shop.TAFKA].includes(item.name) && item.quality > 0) {
+      item.quality -= 1;
+    }
+    if ([Shop.AGED_BRIE, Shop.TAFKA].includes(item.name) && item.quality < 50) {
+      item.quality += 1;
+    }
+
+    if (item.name === Shop.TAFKA) {
+      if (item.sellIn < 11 && item.quality < 50) {
+        item.quality += 1;
+      }
+      if (item.sellIn < 6 && item.quality < 50) {
+        item.quality += 1;
+      }
+    }
+
+    if (item.name !== Shop.SULFURAS) {
+      item.sellIn -= 1;
+    }
+
+    if (item.sellIn < 0) {
+      if (item.name === Shop.AGED_BRIE && item.quality < 50) {
+        item.quality += 1;
+      }
+      if (item.name === Shop.TAFKA) {
+        item.quality = 0;
+      }
+      if (item.name !== Shop.AGED_BRIE && item.name !== Shop.SULFURAS && item.quality > 0) {
+        item.quality -= 1;
+      }
+    }
+    return item;
+  }
+
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
       const item = this.items[i];
-      if (![Shop.AGED_BRIE, Shop.SULFURAS, Shop.TAFKA].includes(item.name) && item.quality > 0) {
-        item.quality -= 1;
-      }
-      if ([Shop.AGED_BRIE, Shop.TAFKA].includes(item.name) && item.quality < 50) {
-        item.quality += 1;
-      }
-
-      if (item.name === Shop.TAFKA) {
-        if (item.sellIn < 11 && item.quality < 50) {
-          item.quality += 1;
-        }
-        if (item.sellIn < 6 && item.quality < 50) {
-          item.quality += 1;
-        }
-      }
-
-      if (item.name !== Shop.SULFURAS) {
-        item.sellIn -= 1;
-      }
-
-      if (item.sellIn < 0) {
-        if (item.name === Shop.AGED_BRIE && item.quality < 50) {
-          item.quality += 1;
-        }
-        if (item.name === Shop.TAFKA) {
-          item.quality = 0;
-        }
-        if (item.name !== Shop.AGED_BRIE && item.name !== Shop.SULFURAS && item.quality > 0) {
-          item.quality -= 1;
-        }
-      }
-      this.items[i] = item;
+      this.items[i] = this.updateItem(item);
     }
 
     return this.items;
