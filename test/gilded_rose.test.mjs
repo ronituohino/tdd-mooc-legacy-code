33d8dc7c9773a7c2f626d871eabda75d15baf6a5
Roni Tuohino <rt@ronituohino.com>
2026-03-20 09:16:39 +0200

    Add coverage tests

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -1,11 +1,48 @@
-import { describe, test } from "vitest";
+import { describe, test, beforeEach } from "vitest";
 import { expect } from "chai";
 import { Item, Shop } from "../src/gilded_rose.mjs";
 
 describe("Gilded Rose", () => {
-  test("foo", () => {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+  test("does something with Aged Brie", () => {
+    const gildedRose = new Shop([
+      new Item("Aged Brie", 0, 0),
+      new Item("Aged Brie", 0, 10),
+      new Item("Aged Brie", 0, 90),
+    ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal("foo");
+    const expectedResult = [new Item("Aged Brie", -1, 2), new Item("Aged Brie", -1, 12), new Item("Aged Brie", -1, 90)];
+    expect(items).to.deep.equal(expectedResult);
+  });
+  test("does something with Sulfuras, Hand of Ragnaros", () => {
+    const gildedRose = new Shop([
+      new Item("Sulfuras, Hand of Ragnaros", 0, 0),
+      new Item("Sulfuras, Hand of Ragnaros", 0, 25),
+      new Item("Sulfuras, Hand of Ragnaros", 0, 100),
+    ]);
+    const items = gildedRose.updateQuality();
+    const expectedResult = [
+      new Item("Sulfuras, Hand of Ragnaros", 0, 0),
+      new Item("Sulfuras, Hand of Ragnaros", 0, 25),
+      new Item("Sulfuras, Hand of Ragnaros", 0, 100),
+    ];
+    expect(items).to.deep.equal(expectedResult);
+  });
+  test("does something with Backstage passes to a TAFKAL80ETC concert", () => {
+    const gildedRose = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 0, 60),
+    ]);
+    const items = gildedRose.updateQuality();
+    const expectedResult = [
+      new Item("Backstage passes to a TAFKAL80ETC concert", -1, 0),
+      new Item("Backstage passes to a TAFKAL80ETC concert", -1, 0),
+    ];
+    expect(items).to.deep.equal(expectedResult);
+  });
+  test("does something with foo", () => {
+    const gildedRose = new Shop([new Item("foo", 0, 0), new Item("foo", 0, 60)]);
+    const items = gildedRose.updateQuality();
+    const expectedResult = [new Item("foo", -1, 0), new Item("foo", -1, 58)];
+    expect(items).to.deep.equal(expectedResult);
   });
 });
